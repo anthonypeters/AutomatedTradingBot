ab0429a0a93c5a418c67b5aac9ac88ec9f7bb5b5
anthonypeters <apeters10@luc.edu>
2021-03-17 16:39:15 -0500

    Cleaned up main

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ import alpacaConnection
 import talib
 import numpy as np
 
+#### Read in Ticker Price History
 data = dataFeed.retrieveTickerPriceHistory('AAPL')
+####
 
+#### Cleanup code to only include closing prices and input into Numpy array
 priceData = []
 i = 0
 
@@ -15,11 +18,16 @@ for n in data["candles"]:
     i += 1
 
 newPriceData = np.asarray(priceData)
+####
 
+#### Compute RSI using talib and find lastRSI and lastClose
 rsi = talib.RSI(newPriceData, timeperiod=14)
 
 lastClose = newPriceData[len(newPriceData)-1]
 lastRSI = rsi[len(rsi)-1]
+####
 
+#### Logic to buy stock 
 if (lastRSI < 50):
     alpacaConnection.create_order(lastClose)
+####
