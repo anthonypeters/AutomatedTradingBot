92134ca8b6d8b4204e4330ce69526c5e68184956
Anthony Peters <1538463+anthonypeters@users.noreply.github.com>
2023-11-05 17:05:05 -0800

    Update alpacaConnection.py

--- alpacaConnection.py
+++ alpacaConnection.py
@@ -3,8 +3,8 @@ import json
 import alpaca_trade_api as tradeapi
 
 #### Needed info to connect to alpaca
-API_KEY = "PKIHF26ARGET0YRSFSHB"
-SECRET_KEY = "n3qvT6B6N3e4zMIWG8gr6pPX740QsbOQsN06togP"
+API_KEY = ""
+SECRET_KEY = ""
 BASE_URL = "https://paper-api.alpaca.markets"
 ACCOUNT_URL = "{}/v2/account".format(BASE_URL)
 POSITIONS_URL = "{}/v2/positions".format(BASE_URL)
